100b21ed753b6dfd3809fa98bb21841c0eb6f00f
isamuels2900 <Samuelsian29@gmail.com>
2024-06-25 15:41:46 -0600

    Created exercise and template models, also the table needed to make a many to many relation between the two.

--- workout_tracker/tracker/models.py
+++ workout_tracker/tracker/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+    
+#Class for each exercise 
+class Exercise(models.Model):
+    name = models.CharField(max_length=100)
+    muscle = models.CharField(max_length=20)
+    category = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.name
+    
+#Class for each template
+class Template(models.Model):
+    name = models.CharField(max_length=100)
+    last_updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    exercises = models.ManyToManyField(Exercise, through='TemplateExercise')
+    
+    def __str__(self):
+        return self.name
+    
+
+class TemplateExercise(models.Model):
+    template = models.ForeignKey(Template, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    sets = models.IntegerField()
+    reps = models.IntegerField()
+    
+    class Meta:
+        unique_together = ('template','exercise')
