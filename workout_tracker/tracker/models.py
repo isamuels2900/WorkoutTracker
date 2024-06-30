80b50aa1f5651f296b7bd0c7b8242d3d844471e8
isamuels2900 <Samuelsian29@gmail.com>
2024-06-30 15:41:26 -0600

    Added a background image to the site and img field to the exercise model.

--- workout_tracker/tracker/models.py
+++ workout_tracker/tracker/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
     
 class Muscle(models.Model):
@@ -24,12 +25,23 @@ class Exercise(models.Model):
     name = models.CharField(max_length=100)
     muscle = models.ForeignKey(Muscle, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    img = models.ImageField(default='defaultExercise.png', upload_to='exercise_pics', null=True)
     
     class Meta:
         ordering = ['muscle']
     
     def __str__(self):
         return self.name
+    
+    def save(self):
+        super().save()
+        
+        image = Image.open(self.img.path)
+        
+        if image.height > 300 or image.width > 300:
+            output_size = (300,300)
+            image.thumbnail(output_size)
+            image.save(self.img.path)
     
 class Template(models.Model):
     name = models.CharField(max_length=100)
