35063c47150d380d56a7f532bf496f33fcfc73ba
isamuels2900 <Samuelsian29@gmail.com>
2024-06-30 17:34:07 -0600

    - Added view exercises - Removed additional features box and moved additional features to the navbar - Website displays elements in the center

--- workout_tracker/tracker/urls.py
+++ workout_tracker/tracker/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='tracker-home'),
     path('about/', views.about, name='tracker-about'),
     path('one_rep_max_calculator/', views.one_rep_max_calc, name='tracker-one_rep_max_calc'),
+    path('exercises/', views.view_exercises, name='tracker-exercises'),
 ]
--- workout_tracker/tracker/views.py
+++ workout_tracker/tracker/views.py
@@ -14,7 +14,7 @@ def home(request):
 def about(request):
     return render(request, 'tracker/about.html', { 'title':'about'})
 
-def one_rep_max_calc(request):
+def one_rep_max_calc(request):  
     if request.method == 'POST':
         form = one_rep_max_calculator_form(request.POST)
         if form.is_valid():
@@ -23,38 +23,38 @@ def one_rep_max_calc(request):
             # Epley formula for 1RM calculation
             one_rep_max = weight * (1 + reps / 30)
             # Round to 2 decimal places
-            one_rep_max = round(one_rep_max, 2)
+            one_rep_max = round(one_rep_max, 1)
             
             # Percentages based on the one rep max to display on the 1rm calculator html 
             one_rep_max95 = one_rep_max * 0.95
-            one_rep_max95 = round(one_rep_max95, 2)
+            one_rep_max95 = round(one_rep_max95, 1)
             
             one_rep_max90 = one_rep_max * 0.90
-            one_rep_max90 = round(one_rep_max90, 2)
+            one_rep_max90 = round(one_rep_max90, 1)
             
             one_rep_max85 = one_rep_max * 0.85
-            one_rep_max85 = round(one_rep_max85, 2)
+            one_rep_max85 = round(one_rep_max85, 1)
             
             one_rep_max80 = one_rep_max * 0.80
-            one_rep_max80 = round(one_rep_max80, 2)
+            one_rep_max80 = round(one_rep_max80, 1)
             
             one_rep_max75 = one_rep_max * 0.75
-            one_rep_max75 = round(one_rep_max75, 2)
+            one_rep_max75 = round(one_rep_max75, 1)
             
             one_rep_max70 = one_rep_max * 0.70
-            one_rep_max70 = round(one_rep_max70, 2)
+            one_rep_max70 = round(one_rep_max70, 1)
             
             one_rep_max65 = one_rep_max * 0.65
-            one_rep_max65 = round(one_rep_max65, 2)
+            one_rep_max65 = round(one_rep_max65, 1)
             
             one_rep_max60 = one_rep_max * 0.60
-            one_rep_max60 = round(one_rep_max60, 2)
+            one_rep_max60 = round(one_rep_max60, 1)
             
             one_rep_max55 = one_rep_max * 0.55
-            one_rep_max55 = round(one_rep_max55, 2)
+            one_rep_max55 = round(one_rep_max55, 1)
             
             one_rep_max50 = one_rep_max * 0.50
-            one_rep_max50 = round(one_rep_max50, 2)
+            one_rep_max50 = round(one_rep_max50, 1)
             
             return render(request, 'tracker/one_rep_max_calculator.html', 
                           {'form':form, 
@@ -72,3 +72,9 @@ def one_rep_max_calc(request):
     else:
         form = one_rep_max_calculator_form()
     return render(request, 'tracker/one_rep_max_calculator.html', {'form':form})
+
+def view_exercises(request):
+    context = {
+        'exercises': Exercise.objects.all()
+    }
+    return render(request, 'tracker/exercises.html',context)
